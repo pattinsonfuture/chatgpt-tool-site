ada5f8b8e90185396643dca895216e006b53b784
pattinson <pattinson@molicity.com>
2023-02-21 00:19:46 +0800

    feat: 🎸 加上useState、useEffect寫入聊天訊息並存到localStorage
    components\ChatRoom.tsx

--- components/ChatRoom.tsx
+++ components/ChatRoom.tsx
@@ -4,23 +4,79 @@ import ChatRoomBotMessage from "./ChatRoomBotMessage";
 import ChatRoomPersonMessage from "./ChatRoomPersonMessage";
 import { motion } from "framer-motion";
 import ChatRoomInput from "./ChatRoomInput";
+import { useEffect, useState } from "react";
+
+interface Message {
+  message: string;
+  isBot: boolean;
+}
+
+// 設定預設對話
+const defaultMessages: Message[] = [
+  { message: "歡迎來到玩轉 A.I. 人工智能！", isBot: true },
+];
 
 function ChatRoom() {
+  // 記錄對話State，並寫到localStorage
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  // 一開始先讀取localStorage的對話紀錄，為空就設定預設對話
+  useEffect(() => {
+    const messages = localStorage.getItem("ChatRoomMessages");
+    if (messages) {
+      setMessages(JSON.parse(messages));
+    } else {
+      writeMessages(defaultMessages);
+    }
+  }, []);
+
+  // 收到人類送出的Submit，更新對話State
+  const handleHumanSubmit = (message: string) => {
+    writeMessages(messages.concat({ message, isBot: false }));
+  };
+
+  // 重置對話State
+  const handleReset = () => {
+    setMessages([]);
+    localStorage.removeItem("ChatRoomMessages");
+    writeMessages(defaultMessages);
+  };
+
+  // 獨立出寫入messages的function，方便重置時使用
+  const writeMessages = (messages: Message[]) => {
+    setMessages(messages);
+    localStorage.setItem("ChatRoomMessages", JSON.stringify(messages));
+  };
+
   return (
     <>
       <motion.div className="min-h-[72vh]">
-        {/* 聊天對話框 */}
+        {/* 聊天對話內容 */}
         <div className="flex-1 mb-5 justify-end flex flex-col h-[72vh] font-mono">
           <div className="flex flex-col space-y-4 overflow-y-auto scrollbar">
-            {/* 人類對話泡泡 */}
-            <ChatRoomPersonMessage message={`你好呀AI!`} />
-            {/* Bot對話泡泡 */}
-            <ChatRoomBotMessage message={`歡迎來到玩轉 A.I. 人工智能！`} />
+            {/* 使用State的對話紀錄 */}
+            {messages.map((message, index) => {
+              if (message.isBot) {
+                return (
+                  <ChatRoomBotMessage key={index} message={message.message} />
+                );
+              } else {
+                return (
+                  <ChatRoomPersonMessage
+                    key={index}
+                    message={message.message}
+                  />
+                );
+              }
+            })}
           </div>
         </div>
       </motion.div>
-      {/* 對話輸入框 */}
-      <ChatRoomInput />
+      {/* 對話輸入Input */}
+      <ChatRoomInput
+        sendMessage={handleHumanSubmit}
+        resetMessage={handleReset}
+      />
     </>
   );
 }
--- components/ChatRoomInput.tsx
+++ components/ChatRoomInput.tsx
@@ -2,23 +2,54 @@
 
 import { motion } from "framer-motion";
 import { fadeIn } from "@/utils/motion";
-import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import { PaperAirplaneIcon, ArrowPathIcon } from "@heroicons/react/24/solid";
 
-function ChatRoomInput() {
+interface ChatRoomInputProps {
+  sendMessage: (message: string) => void;
+  resetMessage: () => void;
+}
+
+function ChatRoomInput({ sendMessage, resetMessage }: ChatRoomInputProps) {
+  // Submit form 功能
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    sendMessage(e.currentTarget.chat.value);
+    // 清空input
+    e.currentTarget.chat.value = "";
+  };
+
+  // Reset form 功能
+  const handleReset = () => {
+    resetMessage();
+  };
+
   return (
     <motion.div
       variants={fadeIn("up", "spring", 0, 1)}
       initial="hidden"
       animate="show"
       className="w-full mx-auto">
-      <form>
-        <label className="sr-only">Your message</label>
+      {/* 點送出按鈕或按Enter，送出value */}
+      <form
+        onSubmit={handleSubmit}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleSubmit(e);
+          }
+        }}>
         <div className="flex items-center py-2 px-3 bg-gray-50 rounded-lg dark:bg-gray-700 shadow-md shadow-black">
+          <button
+            onClick={handleReset}
+            aria-label="reset"
+            type="button"
+            className="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+            <ArrowPathIcon className="w-6 h-6" />
+          </button>
           <textarea
             id="chat"
             rows={1}
             className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Your message..."></textarea>
+            placeholder="來說點什麼..."></textarea>
           <button
             aria-label="submit"
             type="submit"
